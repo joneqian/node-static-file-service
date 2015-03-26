417435d3fa132d2c7cacc74bdbc2f9fbd16541c0
joneqian <qian.qing@aliyun.com>
2015-03-26 10:05:26 +0800

    modify run argments

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
 var logger_normal = require('./components/logger').normal;
 var config = require('./config/config');
 
+global.ASSET_LISTEN = config.Config.ASSET_LISTEN;
+
+if (process.argv.length  === 3) {
+	var arg2 = process.argv.splice(2);
+
+	if (arg2 == "help") {
+		console.log("static file service command:");
+		console.log("help: node main.js help");
+		console.log("run service: node main.js [port]  ----  the default port is " + config.Config.ASSET_LISTEN);
+		process.exit(0);
+	} else if (isNaN(Number(arg2))) {
+		console.log("Unknown command.Plese run 'node main.js help' .");
+		process.exit(0);
+	} else {
+		global.ASSET_LISTEN = Number(arg2);
+	}
+}
+
+if (process.argv.length > 3) {
+	console.log("Too many arguments given.");
+	console.log("Use 'node main.js help'.");
+	process.exit(0);
+};
+
+
 var app = require('pm').createMaster({
 	'pidfile':  __dirname +  'static-file-service.pid',
 	'statusfile':  __dirname + 'status.log'
@@ -29,7 +54,7 @@ app.on('quit', function(name, pid, code, signal) {
 });
 
 app.register('assetService', __dirname + '/components/asset.js', {
-	'listen': [config.Config.ASSET_LISTEN],
+	'listen': [global.ASSET_LISTEN],
 	'children': 2
 });
 
