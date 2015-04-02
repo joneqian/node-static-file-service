32dc83b7e07118ee63592400ad016f9996988e2e
joneqian <qian.qing@aliyun.com>
2015-04-02 16:21:38 +0800

    modify request method

--- src/components/asset.js
+++ src/components/asset.js
@@ -7,87 +7,110 @@ var mime = require('../config/mime').types;
 var config = require('../config/config');
 
 
-worker.on('message', function(data, from, pid){
+worker.on('message', function(data, from, pid) {
     //logger_http.debug(process.pid + ' http server get message from:' + from + '  data:' + data + ' pid:' + pid);
 });
 
 
-var s = require('http').createServer(function (request, response) {
+var s = require('http').createServer(function(request, response) {
     response.setHeader("Server", "Node/V5");
-    var pathname = url.parse(request.url).pathname;
-    var errHandle = function (res) {
-        res.writeHead(404, "Not Found", {'Content-Type': 'text/plain'});
-        res.write("This request URL  was invaild.");
+    var errHandle = function(res, error) {
+        var err_info = error || 'This request URL  was invaild.'
+        res.writeHead(404, 'Not Found', {
+            'Content-Type': 'text/plain'
+        });
+        res.write(err_info);
         res.end();
     };
 
-    if (pathname.slice(-1) === "/") {
-        errHandle(response);
+    if (request.method !== 'POST') {
+        errHandle(response, 'request method error');
         return;
     }
 
-    var realPath = path.join(config.Config.ASSET_PATH, path.normalize(pathname.replace(/\.\./g, "")));
-    var pathHandle = function (realPath) {
-        fs.stat(realPath, function (err, stats) {
+    var url = request.url.split('?').shift().split('/')[1].toLowerCase();
+    var buffer = '';
+
+    request.on('data', function(data) {
+        buffer += data;
+    });
+
+    request.on('end', function() {
+        var realPath = config.Config.ASSET_PATH + url + '/';
+        switch (url) {
+            case 'device_catalog':
+                realPath += config.Default.device_file;
+                break;
+            case 'screen_dump':
+                try {
+                    var obj = JSON.parse(buffer);
+                    if (obj['name'] === undefined || obj['type'] === undefined) {
+                        errHandle(response, 'data format error');
+                        return;
+                    }
+                    realPath += obj['name'] + '.' + obj['type'];
+                } catch (e) {
+                    logger_asset.error('json parse error');
+                    errHandle(response, 'data format error');
+                    return;
+                }
+                break;
+            default:
+                errHandle(response, 'command error');
+                return;
+        }
+        pathHandle(realPath);
+    });
+
+    var pathHandle = function(realPath) {
+        fs.stat(realPath, function(err, stats) {
             if (err) {
+               logger_asset.error('fs error:' + err);
                 errHandle(response);
             } else {
-                if (stats.isDirectory()) {
-                    if (realPath.indexOf("device_catalog") > 0) {
-                        realPath = path.join(realPath, "/", config.Default.device_file);        
-                    } else if (realPath.indexOf("screen_dump") > 0) {
-                        realPath = path.join(realPath, "/", config.Default.screen_file);        
-                    } else {
-                        realPath = path.join(realPath, "/", config.Default.file);
-                    }
-                    pathHandle(realPath);
+                var ext = path.extname(realPath);
+                ext = ext ? ext.slice(1) : 'unknown';
+                var contentType = mime[ext] || "text/plain";
+                response.setHeader("Content-Type", contentType);
+                response.setHeader('Content-Length', stats.size);
+
+                var lastModified = stats.mtime.toUTCString();
+                var ifModifiedSince = "If-Modified-Since".toLowerCase();
+                response.setHeader("Last-Modified", lastModified);
+
+                if (ext.match(config.Expires.fileMatch)) {
+                    var expires = new Date();
+                    expires.setTime(expires.getTime() + config.Expires.maxAge * 1000);
+                    response.setHeader("Expires", expires.toLocaleString());
+                    response.setHeader("Cache-Control", "max-age=" + config.Expires.maxAge);
+                }
+
+                if (request.headers[ifModifiedSince] && lastModified == request.headers[ifModifiedSince]) {
+                    response.writeHead(304, "Not Modified");
+                    response.end();
                 } else {
-                    var ext = path.extname(realPath);
-                    ext = ext ? ext.slice(1) : 'unknown';
-                    var contentType = mime[ext] || "text/plain";
-                    response.setHeader("Content-Type", contentType);
-                    response.setHeader('Content-Length', stats.size);
+                    var compressHandle = function(raw, statusCode, reasonPhrase) {
+                        var stream = raw;
+                        response.writeHead(statusCode, reasonPhrase);
+                        stream.pipe(response);
+                    };
 
-                    var lastModified = stats.mtime.toUTCString();
-                    var ifModifiedSince = "If-Modified-Since".toLowerCase();
-                    response.setHeader("Last-Modified", lastModified);
-
-                    if (ext.match(config.Expires.fileMatch)) {
-                        var expires = new Date();
-                        expires.setTime(expires.getTime() + config.Expires.maxAge * 1000);
-                        response.setHeader("Expires", expires.toUTCString());
-                        response.setHeader("Cache-Control", "max-age=" + config.Expires.maxAge);
-                    }
-
-                    if (request.headers[ifModifiedSince] && lastModified == request.headers[ifModifiedSince]) {
-                        response.writeHead(304, "Not Modified");
-                        response.end();
-                    } else {
-                        var compressHandle = function (raw, statusCode, reasonPhrase) {
-                                var stream = raw;
-                                response.writeHead(statusCode, reasonPhrase);
-                                stream.pipe(response);
-                            };
-
-                        var raw = fs.createReadStream(realPath);
-                        compressHandle(raw, 200);
-                    }
+                    var raw = fs.createReadStream(realPath);
+                    compressHandle(raw, 200);
                 }
             }
         });
     };
-
-    pathHandle(realPath);
 });
 
 worker.ready(function(socket, which) {
     s.emit('connection', socket);
 });
 
-worker.on('suicide', function (by) {
+worker.on('suicide', function(by) {
     logger_asset.info('suicide by ' + by);
 });
 
-process.on('uncaughtException', function (err) {
+process.on('uncaughtException', function(err) {
     logger_asset.error('Caught Exception:' + err);
 });
--- src/config/config.js
+++ src/config/config.js
@@ -10,7 +10,7 @@ exports.Compress = {
 exports.Default = {
     file: 'index.html',
     screen_file: 'screen_default.jpg',
-    device_file: 'virtual_default_list.json'
+    device_file: 'virtual_list.json'
 };
 exports.Timeout = 20 * 60 * 1000;
 exports.Secure = null;
@@ -18,5 +18,5 @@ exports.Secure = null;
 exports.Config = {
     LOG_PATH: path.dirname(__dirname) + '/logs/log',
     ASSET_LISTEN: 1200,
-    ASSET_PATH: path.dirname(__dirname) + '/assets'
+    ASSET_PATH: path.dirname(__dirname) + '/assets/'
 };
